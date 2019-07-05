c08b43daa613501cad10c0d275789e4db741ef96
trifan <trifan@adobe.com>
2019-07-05 16:07:26 +0300

    refactoring'

--- pkg/api/handler/xds.go
+++ pkg/api/handler/xds.go
@@ -1,71 +1,358 @@
 package handler
 
 import (
+	"bufio"
 	"context"
+	"flag"
+	"fmt"
+	myals "github.com/atrifan/envoy-plane/pkg/api/acesslogs"
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
+	"github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
+	accesslog "github.com/envoyproxy/go-control-plane/envoy/service/accesslog/v2"
+	discovery "github.com/envoyproxy/go-control-plane/envoy/service/discovery/v2"
+	"github.com/envoyproxy/go-control-plane/pkg/cache"
+	xds "github.com/envoyproxy/go-control-plane/pkg/server"
+	"github.com/envoyproxy/go-control-plane/pkg/util"
+	log "github.com/sirupsen/logrus"
+	hcm "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
+	"google.golang.org/grpc"
+	"net"
+	"net/http"
+	"os"
+	"sync"
+	"sync/atomic"
+	"time"
 )
 
-type (
-	Callbacks interface {
-		// OnStreamOpen is called once an xDS stream is open with a stream ID and the type URL (or "" for ADS).
-		// Returning an error will end processing and close the stream. OnStreamClosed will still be called.
-		OnStreamOpen(context.Context, int64, string) error
-		// OnStreamClosed is called immediately prior to closing an xDS stream with a stream ID.
-		OnStreamClosed(int64)
-		// OnStreamRequest is called once a request is received on a stream.
-		// Returning an error will end processing and close the stream. OnStreamClosed will still be called.
-		OnStreamRequest(int64, *v2.DiscoveryRequest) error
-		// OnStreamResponse is called immediately prior to sending a response on a stream.
-		OnStreamResponse(int64, *v2.DiscoveryRequest, *v2.DiscoveryResponse)
-		// OnFetchRequest is called for each Fetch request. Returning an error will end processing of the
-		// request and respond with an error.
-		OnFetchRequest(context.Context, *v2.DiscoveryRequest) error
-		// OnFetchResponse is called immediately prior to sending a response.
-		OnFetchResponse(*v2.DiscoveryRequest, *v2.DiscoveryResponse)
-	}
+var (
+	debug       bool
+	onlyLogging bool
 
-	NodeHash interface {
-		ID(node *core.Node) string
-	}
+	localhost = "127.0.0.1"
 
-	ControlPlane struct {}
+	port        uint
+	gatewayPort uint
+	alsPort     uint
 
-	ControlPlaneHash struct {}
+	mode string
+
+	version int32
+
+	config cache.SnapshotCache
 )
 
-func (handler *ControlPlane) OnStreamOpen(context.Context, int64, string) error {
+const (
+	XdsCluster = "xds_cluster"
+	Ads        = "ads"
+	Xds        = "xds"
+	Rest       = "rest"
+)
+
+func init() {
+	flag.BoolVar(&debug, "debug", true, "Use debug logging")
+	flag.BoolVar(&onlyLogging, "onlyLogging", false, "Only demo AccessLogging Service")
+	flag.UintVar(&port, "port", 18000, "Management server port")
+	flag.UintVar(&gatewayPort, "gateway", 18001, "Management server port for HTTP gateway")
+	flag.UintVar(&alsPort, "als", 18090, "Accesslog server port")
+	flag.StringVar(&mode, "ads", Ads, "Management server type (ads, xds, rest)")
+}
+
+type logger struct{}
+
+func (logger logger) Infof(format string, args ...interface{}) {
+	log.Infof(format, args...)
+}
+func (logger logger) Errorf(format string, args ...interface{}) {
+	log.Errorf(format, args...)
+}
+func (cb *callbacks) Report() {
+	cb.mu.Lock()
+	defer cb.mu.Unlock()
+	log.WithFields(log.Fields{"fetches": cb.fetches, "requests": cb.requests}).Info("cb.Report()  callbacks")
+}
+func (cb *callbacks) OnStreamOpen(ctx context.Context, id int64, typ string) error {
+	log.Infof("OnStreamOpen %d open for %s", id, typ)
 	return nil
 }
 
-func (handler *ControlPlane) OnStreamClosed(int64) {
-	return
+func (cb *callbacks) OnStreamClosed(id int64) {
+	log.Infof("OnStreamClosed %d closed", id)
 }
+func (cb *callbacks) OnStreamRequest(int64, *v2.DiscoveryRequest) error {
+	log.Infof("OnStreamRequest")
+	cb.mu.Lock()
+	defer cb.mu.Unlock()
+	cb.requests++
+	if cb.signal != nil {
+		close(cb.signal)
+		cb.signal = nil
+	}
 
-func (handler *ControlPlane) OnStreamRequest(int64, *v2.DiscoveryRequest) error {
 	return nil
 }
-
-func (handler *ControlPlane) OnFetchRequest(context.Context, *v2.DiscoveryRequest) error {
+func (cb *callbacks) OnStreamResponse(int64, *v2.DiscoveryRequest, *v2.DiscoveryResponse) {
+	log.Infof("OnStreamResponse...")
+	cb.Report()
+}
+func (cb *callbacks) OnFetchRequest(ctx context.Context, req *v2.DiscoveryRequest) error {
+	log.Infof("OnFetchRequest...")
+	cb.mu.Lock()
+	defer cb.mu.Unlock()
+	cb.fetches++
+	if cb.signal != nil {
+		close(cb.signal)
+		cb.signal = nil
+	}
 	return nil
 }
+func (cb *callbacks) OnFetchResponse(*v2.DiscoveryRequest, *v2.DiscoveryResponse) {}
 
-func (handler *ControlPlane) OnStreamResponse(int64, *v2.DiscoveryRequest, *v2.DiscoveryResponse) {
-	return
+type callbacks struct {
+	signal   chan struct{}
+	fetches  int
+	requests int
+	mu       sync.Mutex
 }
 
-func (handler *ControlPlane) OnFetchResponse(*v2.DiscoveryRequest, *v2.DiscoveryResponse) {
-	return
+// Hasher returns node ID as an ID
+type Hasher struct {
 }
 
-func (handler *ControlPlaneHash) ID(node *core.Node) string {
-	return "someHash"
+// ID function
+func (h Hasher) ID(node *core.Node) string {
+	if node == nil {
+		return "unknown"
+	}
+	return node.Id
 }
 
-func NewControlPlaneXDS() Callbacks {
-	return &ControlPlane{}
+// RunAccessLogServer starts an accesslog service.
+func RunAccessLogServer(ctx context.Context, als *myals.AccessLogService, port uint) {
+	grpcServer := grpc.NewServer()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.WithError(err).Fatal("failed to listen")
+	}
+
+	accesslog.RegisterAccessLogServiceServer(grpcServer, als)
+	log.WithFields(log.Fields{"port": port}).Info("access log server listening")
+
+	go func() {
+		if err = grpcServer.Serve(lis); err != nil {
+			log.Error(err)
+		}
+	}()
+	<-ctx.Done()
+
+	grpcServer.GracefulStop()
 }
 
-func NewHashFunction() NodeHash {
-	return &ControlPlaneHash{}
+const grpcMaxConcurrentStreams = 1000000
+
+// RunManagementServer starts an xDS server at the given port.
+func RunManagementServer(ctx context.Context, server xds.Server, port uint) {
+	var grpcOptions []grpc.ServerOption
+	grpcOptions = append(grpcOptions, grpc.MaxConcurrentStreams(grpcMaxConcurrentStreams))
+	grpcServer := grpc.NewServer(grpcOptions...)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.WithError(err).Fatal("failed to listen")
+	}
+
+	// register services
+	discovery.RegisterAggregatedDiscoveryServiceServer(grpcServer, server)
+	v2.RegisterEndpointDiscoveryServiceServer(grpcServer, server)
+	v2.RegisterClusterDiscoveryServiceServer(grpcServer, server)
+	v2.RegisterRouteDiscoveryServiceServer(grpcServer, server)
+	v2.RegisterListenerDiscoveryServiceServer(grpcServer, server)
+
+	log.WithFields(log.Fields{"port": port}).Info("management server listening")
+	go func() {
+		if err = grpcServer.Serve(lis); err != nil {
+			log.Error(err)
+		}
+	}()
+	<-ctx.Done()
+
+	grpcServer.GracefulStop()
 }
+
+// RunManagementGateway starts an HTTP gateway to an xDS server.
+func RunManagementGateway(ctx context.Context, srv xds.Server, port uint) {
+	log.WithFields(log.Fields{"port": port}).Info("gateway listening HTTP/1.1")
+	server := &http.Server{Addr: fmt.Sprintf(":%d", port), Handler: &xds.HTTPGateway{Server: srv}}
+	go func() {
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			// NOTE: there is a chance that next line won't have time to run,
+			// as main() doesn't wait for this goroutine to stop. don't use
+			// code with race conditions like these for production. see post
+			// comments below on more discussion on how to handle this.
+			log.Fatalf("ListenAndServe(): %s", err)
+		}
+	}()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Error(err)
+	}
+}
+
+
+func InitXds(ctx context.Context) {
+	flag.Parse()
+	if debug {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	signal := make(chan struct{})
+	cb := &callbacks{
+		signal:   signal,
+		fetches:  0,
+		requests: 0,
+	}
+	config = cache.NewSnapshotCache(mode == Ads, Hasher{}, logger{})
+
+	srv := xds.NewServer(config, cb)
+
+	//als := &accesslogs.AccessLogService{}
+	als := &myals.AccessLogService{}
+	go RunAccessLogServer(ctx, als, alsPort)
+
+	if onlyLogging {
+		cc := make(chan struct{})
+		<-cc
+		os.Exit(0)
+	}
+
+	// start the xDS server
+	go RunManagementServer(ctx, srv, port)
+	go RunManagementGateway(ctx, srv, gatewayPort)
+
+	<-signal
+
+	als.Dump(func(s string) { log.Debug(s) })
+	cb.Report()
+
+	for {
+		atomic.AddInt32(&version, 1)
+		nodeId := config.GetStatusKeys()[0]
+
+		var clusterName = "service_bbc"
+		var remoteHost = "www.bbc.com"
+		var sni = "www.bbc.com"
+		log.Infof(">>>>>>>>>>>>>>>>>>> creating cluster " + clusterName)
+
+		//c := []cache.Resource{resource.MakeCluster(resource.Ads, clusterName)}
+
+		h := &core.Address{Address: &core.Address_SocketAddress{
+			SocketAddress: &core.SocketAddress{
+				Address:  remoteHost,
+				Protocol: core.TCP,
+				PortSpecifier: &core.SocketAddress_PortValue{
+					PortValue: uint32(443),
+				},
+			},
+		}}
+
+		c := []cache.Resource{
+			&v2.Cluster{
+				Name:            clusterName,
+				ConnectTimeout:  2 * time.Second,
+				ClusterDiscoveryType: &v2.Cluster_Type{v2.Cluster_LOGICAL_DNS},
+				DnsLookupFamily: v2.Cluster_V4_ONLY,
+				LbPolicy:        v2.Cluster_ROUND_ROBIN,
+				Hosts:           []*core.Address{h},
+				TlsContext: &auth.UpstreamTlsContext{
+					Sni: sni,
+				},
+			},
+		}
+
+		// =================================================================================
+		var listenerName = "listener_0"
+		var targetHost = "www.bbc.com"
+		var targetRegex = "/api"
+		var virtualHostName = "local_service"
+		var routeConfigName = "local_route"
+
+		log.Infof(">>>>>>>>>>>>>>>>>>> creating listener " + listenerName)
+
+		v := route.VirtualHost{
+			Name:    virtualHostName,
+			Domains: []string{"*"},
+
+			Routes: []route.Route{{
+				Match: route.RouteMatch{
+					PathSpecifier: &route.RouteMatch_Prefix{
+						Prefix: targetRegex,
+					},
+				},
+				Action: &route.Route_Route{
+					Route: &route.RouteAction{
+						HostRewriteSpecifier: &route.RouteAction_HostRewrite{
+							HostRewrite: targetHost,
+						},
+						ClusterSpecifier: &route.RouteAction_Cluster{
+							Cluster: clusterName,
+						},
+						PrefixRewrite: "/robots.txt",
+					},
+				},
+			}}}
+
+		manager := &hcm.HttpConnectionManager{
+			CodecType:  hcm.AUTO,
+			StatPrefix: "ingress_http",
+			RouteSpecifier: &hcm.HttpConnectionManager_RouteConfig{
+				RouteConfig: &v2.RouteConfiguration{
+					Name:         routeConfigName,
+					VirtualHosts: []route.VirtualHost{v},
+				},
+			},
+			HttpFilters: []*hcm.HttpFilter{{
+				Name: util.Router,
+			}},
+		}
+		pbst, err := util.MessageToStruct(manager)
+		if err != nil {
+			panic(err)
+		}
+
+		var l = []cache.Resource{
+			&v2.Listener{
+				Name: listenerName,
+				Address: core.Address{
+					Address: &core.Address_SocketAddress{
+						SocketAddress: &core.SocketAddress{
+							Protocol: core.TCP,
+							Address:  localhost,
+							PortSpecifier: &core.SocketAddress_PortValue{
+								PortValue: 9191,
+							},
+						},
+					},
+				},
+				FilterChains: []listener.FilterChain{{
+					Filters: []listener.Filter{{
+						Name:   util.HTTPConnectionManager,
+						ConfigType: &listener.Filter_Config{pbst},
+					}},
+				}},
+			}}
+
+		// =================================================================================
+
+		log.Infof(">>>>>>>>>>>>>>>>>>> creating snapshot Version " + fmt.Sprint(version))
+		snap := cache.NewSnapshot(fmt.Sprint(version), nil, c, nil, l)
+
+		config.SetSnapshot(nodeId, snap)
+
+		reader := bufio.NewReader(os.Stdin)
+		_, _ = reader.ReadString('\n')
+
+	}
+
+}
+
